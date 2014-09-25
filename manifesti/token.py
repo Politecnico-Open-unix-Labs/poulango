f7d65cf48f5d6ba560371ab3fd8727502926ba18
Stefano Sanfilippo <a.little.coder@gmail.com>
2014-09-25 17:20:27 +0200

    Fixato secure_compare

--- manifesti/token.py
+++ manifesti/token.py
@@ -10,7 +10,7 @@ def secure_compare(a, b):
 
     Python 2.7.7 ha `hmac.compare_digest` ma non possiamo usarlo ancora:(
     """
-    return sum(i == j for i, j in zip(a, b)) > 0
+    return sum(1 for i, j in zip(a, b) if i != j) == 0
 
 
 def calculate_token(id):
