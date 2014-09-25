3ff469d1f15bcde55805203f1752a2012b69fc96
Stefano Sanfilippo <a.little.coder@gmail.com>
2014-09-25 14:46:38 +0200

    Bulk reset per lo stato delle bacheche.

--- manifesti/admin.py
+++ manifesti/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from manifesti.models import Posizione
 
 
+def reset_fatto(modeladmin, request, queryset):
+    queryset.update(fatto=False)
+
+reset_fatto.short_description = "Riporta bacheca a 'non fatto'"
+
 class PosizioneAdmin(admin.ModelAdmin):
     list_display = ("descrizione", "fatto", "token", "latitudine", "longitudine", "ultima_visita")
+    actions = (reset_fatto, )
 
 admin.site.register(Posizione, PosizioneAdmin)
